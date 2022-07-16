932b3f942805392d75ae59c8437b96b17a28c4c7
YasinKaradag07 <yasinkaradag0607@gmail.com>
2022-07-16 16:27:10 +0300

    initial commit

---
+++ src/test/java/com/cydeo/day01/SimpleGetRequest.java
@@ -0,0 +1,20 @@
+package com.cydeo.day01;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    String url = "http://100.26.201.240:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+
+
+
+
+
+    }
+
+
+}
