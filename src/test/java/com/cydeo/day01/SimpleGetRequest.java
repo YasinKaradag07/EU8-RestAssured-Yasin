e98db5f67abe8afdf63006c1dc6d2e307cffd134
YasinKaradag07 <yasinkaradag0607@gmail.com>
2022-07-16 16:34:49 +0300

    get response from api

--- src/test/java/com/cydeo/day01/SimpleGetRequest.java
+++ src/test/java/com/cydeo/day01/SimpleGetRequest.java
@@ -1,5 +1,7 @@
 package com.cydeo.day01;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class SimpleGetRequest {
@@ -9,7 +11,14 @@ public class SimpleGetRequest {
     @Test
     public void test1(){
 
+        // send a get request and save response inside the response object
+        Response response = RestAssured.get(url);
 
+        // print response status code
+        System.out.println(response.statusCode());
+
+        // print response body
+        response.prettyPrint();
 
 
 
