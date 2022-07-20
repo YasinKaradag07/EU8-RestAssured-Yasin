9ba04cfa3aa8e90e9eb9c6bb3e3b9c21e29ecb21
YasinKaradag07 <yasinkaradag0607@gmail.com>
2022-07-20 18:28:34 +0300

    DBUtils added, adjusted the SpartanTestBase

--- src/test/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanTestBase.java
@@ -10,5 +10,11 @@ public abstract class SpartanTestBase {
     public static void init(){
         // save baseUrl inside this variable so that we don't need to type each http method.
         baseURI = "http://100.26.201.240:8000";
+
+        String dbUrl = "jdbc:oracle:thin:@44.202.119.26:1521:xe";
+        String dbUsername = "SP";
+        String dbPassword = "SP";
+
+        //DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
     }
 }
