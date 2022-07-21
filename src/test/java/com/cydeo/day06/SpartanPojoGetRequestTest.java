ff0dab5d9ba07f4f6380f1ef072c9423b0d56dcf
YasinKaradag07 <yasinkaradag0607@gmail.com>
2022-07-21 14:56:24 +0300

    ORDSApiWithJsonPath examples are implemented and pojo classes created

--- src/test/java/com/cydeo/day06/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cydeo/day06/SpartanPojoGetRequestTest.java
@@ -1,5 +1,6 @@
 package com.cydeo.day06;
 
+import com.cydeo.pojo.Search;
 import com.cydeo.pojo.Spartan;
 import com.cydeo.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
@@ -7,6 +8,8 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -67,6 +70,37 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
         System.out.println("s1.getName() = " + s1.getName());
         System.out.println("s1.getGender() = " + s1.getGender());
 
+    }
+
+    @Test
+    public void test3(){
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParams("nameContains", "a",
+                        "gender", "Male")
+                .when().get("/api/spartans/search")
+                .then().statusCode(200)
+                .extract().response();
+
+        Search searchResult = response.as(Search.class);
+
+        System.out.println(searchResult.getContent().get(0).getName());
+    }
+
+    @DisplayName("GET  /spartans/search and save as List<Spartan>")
+    @Test
+    public void test4(){
+        List<Spartan> spartanList = given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParams("nameContains", "a",
+                        "gender", "Male")
+                .when()
+                .get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .extract().jsonPath().getList("items", Spartan.class);
+
+        System.out.println(spartanList.get(1).getName());
 
     }
 
